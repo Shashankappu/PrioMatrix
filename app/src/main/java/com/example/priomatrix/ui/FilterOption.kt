adbd4f62e3a5b65901564ea27e39131d0476ab39
Shashankappu <Shashankprince523@gmail.com>
2025-12-28 15:43:56 +0530

    Added UI changes for TaskListScreen

--- app/src/main/java/com/example/priomatrix/ui/FilterOption.kt
+++ app/src/main/java/com/example/priomatrix/ui/FilterOption.kt
@@ -3,13 +3,13 @@ package com.example.priomatrix.ui
 sealed class FilterOption(val title: String) {
 
     sealed class Completion(title: String) : FilterOption(title) {
-        object All : Completion("All tasks")
+        object All : Completion("All")
         object Completed : Completion("Completed")
         object Incomplete : Completion("Incomplete")
     }
 
     sealed class Name(title: String) : FilterOption(title) {
-        object AtoZ : Name("Name (A → Z)")
-        object ZtoA : Name("Name (Z → A)")
+        object AtoZ : Name("(A → Z)")
+        object ZtoA : Name("(Z → A)")
     }
 }
