5161b85a7abf768715837c8bd17dcbff319cae45
Shashankappu <Shashankprince523@gmail.com>
2025-12-28 15:28:19 +0530

    Added Styling to TaskItem

--- app/src/main/java/com/example/priomatrix/ui/TaskStatus.kt
+++ app/src/main/java/com/example/priomatrix/ui/TaskStatus.kt
@@ -1,7 +1,20 @@
 package com.example.priomatrix.ui
 
+import androidx.compose.ui.graphics.Color
+import com.example.priomatrix.ui.theme.TaskStatusCompletedColor
+import com.example.priomatrix.ui.theme.TaskStatusInProgressColor
+import com.example.priomatrix.ui.theme.TaskStatusPendingColor
+
 enum class TaskStatus {
     PENDING,        // Task not started yet
     IN_PROGRESS,    // Task currently in progress
     COMPLETED       // Task finished
+}
+
+fun TaskStatus.indicatorColor(): Color {
+    return when (this) {
+        TaskStatus.PENDING -> TaskStatusPendingColor
+        TaskStatus.IN_PROGRESS -> TaskStatusInProgressColor
+        TaskStatus.COMPLETED -> TaskStatusCompletedColor
+    }
 }
